8e27b4a80b2f4264eb59b13d00ad45e34a54b7de
MuhammadAyub24 <43295653+MuhammadAyub24@users.noreply.github.com>
2019-10-07 19:38:41 -0400

    new style

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+let logo = document.querySelector(`#log`);
+let listz = document.querySelector(`.chng`);
+
+
+logo.addEventListener(`click`, () =>
+{
+    document.querySelector(`.inp`).style.display = 'block';
+    document.querySelector(`.srch`).style.display = 'block';
+
+    logo.style.display = 'none';
+
+    listz.style.padding = '20px';
+})
